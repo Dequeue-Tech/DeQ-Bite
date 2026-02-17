9e2b26f1fddd62fceb745235c16891bd919bd246
Sumit Shekhar <shekharsumit65@gmail.com>
2026-02-17 12:25:39 +0530

    Fixed vercel build error 3

--- restaurant-backend/api/index.js
+++ restaurant-backend/api/index.js
@@ -1,30 +1,3 @@
-const path = require('path');
-const Module = require('module');
-
-const aliasBaseCandidates = [
-  path.join(__dirname, '../dist'),
-  path.join(__dirname, '../src'),
-  path.join(process.cwd(), 'dist'),
-  path.join(process.cwd(), 'src'),
-];
-
-const originalResolveFilename = Module._resolveFilename;
-Module._resolveFilename = function resolveWithAtAlias(request, parent, isMain, options) {
-  if (typeof request === 'string' && request.startsWith('@/')) {
-    const suffix = request.slice(2);
-    for (const basePath of aliasBaseCandidates) {
-      try {
-        return originalResolveFilename.call(this, path.join(basePath, suffix), parent, isMain, options);
-      } catch (error) {
-        if (!error || error.code !== 'MODULE_NOT_FOUND') {
-          throw error;
-        }
-      }
-    }
-  }
-  return originalResolveFilename.call(this, request, parent, isMain, options);
-};
-
 function requireFirst(paths) {
   const notFoundErrors = [];
   for (const modulePath of paths) {
@@ -45,18 +18,14 @@ function requireFirst(paths) {
 const appModule = requireFirst([
   '../dist/app',
   '../dist/src/app',
-  '../src/app',
   '../../dist/app',
   '../../dist/src/app',
-  '../../src/app',
 ]);
 const dbModule = requireFirst([
   '../dist/config/database',
   '../dist/src/config/database',
-  '../src/config/database',
   '../../dist/config/database',
   '../../dist/src/config/database',
-  '../../src/config/database',
 ]);
 
 const app = appModule.default || appModule;
